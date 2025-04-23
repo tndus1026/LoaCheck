4231ec6cf993abd5575994d8be666acb960310dd
syeon <oks456@naver.com>
2025-04-23 18:44:36 +0900

    프로젝트 초기 커밋

---
+++ LoaCheck/src/main/java/com/loacheck/LoaCheckApplication.java
@@ -0,0 +1,13 @@
+package com.loacheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoaCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoaCheckApplication.class, args);
+	}
+
+}
